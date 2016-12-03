9220a3d002cf484e7d788272c3fac10bf4da3c10
Philipp Hagemeister <phihag@phihag.de>
2016-12-03 22:42:53 +0100

    test for incorrect umwertung

---
+++ src/checks/umwertung.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = function*(season, data) {
+	for (const pm of data.playermatches) {
+		if (pm.flag_umwertung_gegen_team1 && (pm.setcount === 2) &&
+				(pm.set1team1 === 0) && (pm.set1team2 === 21) &&
+				(pm.set2team1 === 0) && (pm.set2team2 === 21)) {
+			yield {
+				teammatch_id: pm.teammatchid,
+				match_id: pm.matchid,
+				message: 'Punktestand bei Umwertung gelöscht.',
+			};
+		}
+
+		if (pm.flag_umwertung_gegen_team2 && (pm.setcount === 2) &&
+				(pm.set1team1 === 21) && (pm.set1team2 === 0) &&
+				(pm.set2team1 === 21) && (pm.set2team2 === 0)) {
+			yield {
+				teammatch_id: pm.teammatchid,
+				match_id: pm.matchid,
+				message: 'Punktestand bei Umwertung gelöscht.',
+			};
+		}
+	}
+};

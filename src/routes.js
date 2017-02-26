91b5cba4cee1d4184ba923c716b260755b243712
Philipp Hagemeister <phihag@phihag.de>
2017-02-26 21:31:22 +0100

    prepare for public installation

--- src/routes.js
+++ src/routes.js
@@ -16,38 +16,38 @@ function setup(app) {
 	app.use(favicon(path.dirname(__dirname) + '/static/favicon.ico'));
 
 	app.get('/', root.root_handler);
-
 	app.post('/login', users.login_handler);
-	app.get('/user/me', users.me_handler);
-	app.get('/user/create_dialog', users.create_dialog_handler);
-	app.post('/user/create', users.create_handler);
-	app.get('/change-password', users.change_password_handler);
-	app.post('/logout', users.logout_handler);
 
-	app.get('/season_add/dialog', seasons.add_dialog_handler);
-	app.post('/season_add', seasons.add_handler);
-	app.get('/s/:season_key/', seasons.show_handler);
-	app.post('/s/:season_key/recheck', seasons.recheck_handler);
-	app.get('/s/:season_key/check', seasons.check_handler);
-	app.post('/s/:season_key/change', seasons.change_handler);
-	app.get('/s/:season_key/problems', seasons.show_problems_handler);
-	app.post('/s/:season_key/ignore', seasons.ignore_handler);
-	app.post('/s/:season_key/unignore', seasons.unignore_handler);
-	app.post('/s/:season_key/receiver_add', seasons.receiver_add_handler);
-	app.post('/s/:season_key/receiver_delete', seasons.receiver_delete_handler);
-	app.post('/s/:season_key/preview', seasons.email_preview);
-	app.post('/s/:season_key/send', seasons.email_send);
+	app.get('/user/me', users.need_permission('any', users.me_handler));
+	app.get('/user/create_dialog', users.need_permission('any', users.create_dialog_handler));
+	app.post('/user/create', users.need_permission('any', users.create_handler));
+	app.get('/change-password', users.need_permission('any', users.change_password_handler));
+	app.post('/logout', users.need_permission('any', users.logout_handler));
 
-	app.post('/s/:season_key/autorun/create', autoruns.create_handler);
-	app.post('/s/:season_key/autorun/:autorun_id/delete', autoruns.delete_handler);
-	app.post('/s/:season_key/autorun/:autorun_id/receiver_add', autoruns.receiver_add_handler);
-	app.post('/s/:season_key/autorun/:autorun_id/receiver_delete', autoruns.receiver_delete_handler);
+	app.get('/season_add/dialog', users.need_permission('any', seasons.add_dialog_handler));
+	app.post('/season_add', users.need_permission('any', seasons.add_handler));
+	app.get('/s/:season_key/', users.need_permission('any', seasons.show_handler));
+	app.post('/s/:season_key/recheck', users.need_permission('any', seasons.recheck_handler));
+	app.get('/s/:season_key/check', users.need_permission('any', seasons.check_handler));
+	app.post('/s/:season_key/change', users.need_permission('any', seasons.change_handler));
+	app.get('/s/:season_key/problems', users.need_permission('any', seasons.show_problems_handler));
+	app.post('/s/:season_key/ignore', users.need_permission('any', seasons.ignore_handler));
+	app.post('/s/:season_key/unignore', users.need_permission('any', seasons.unignore_handler));
+	app.post('/s/:season_key/receiver_add', users.need_permission('any', seasons.receiver_add_handler));
+	app.post('/s/:season_key/receiver_delete', users.need_permission('any', seasons.receiver_delete_handler));
+	app.post('/s/:season_key/preview', users.need_permission('any', seasons.email_preview));
+	app.post('/s/:season_key/send', users.need_permission('any', seasons.email_send));
+
+	app.post('/s/:season_key/autorun/create', users.need_permission('any', autoruns.create_handler));
+	app.post('/s/:season_key/autorun/:autorun_id/delete', users.need_permission('any', autoruns.delete_handler));
+	app.post('/s/:season_key/autorun/:autorun_id/receiver_add', users.need_permission('any', autoruns.receiver_add_handler));
+	app.post('/s/:season_key/autorun/:autorun_id/receiver_delete', users.need_permission('any', autoruns.receiver_delete_handler));
 
 
-	app.get('/s/:season_key/show/', show.season_handler);
-	app.get('/s/:season_key/show/player/:player_id', show.player_handler);
+	app.get('/s/:season_key/show/', users.need_permission('any', show.season_handler));
+	app.get('/s/:season_key/show/player/:player_id', users.need_permission('any', show.player_handler));
 
-	app.post('/s/:season_key/download-start', downloads.start_handler);
+	app.post('/s/:season_key/download-start', users.need_permission('any', downloads.start_handler));
 }
 
 module.exports = {

2ed178e5126a287982f21986bc106d9cd5dae47f
Philipp Hagemeister <phihag@phihag.de>
2017-03-31 11:21:47 +0200

    lint

--- src/checks/verlegung.js
+++ src/checks/verlegung.js
@@ -3,7 +3,7 @@
 const utils = require('../utils');
 
 const DATE_RE = utils.multilineRegExp([
-	/([0-9]{1,2}\.\s*(?:Januar|Februar|März|April|Mai|Juni|Juli|August|September|Oktober|November|Dezember|[0-9]{1,2})\.\s*[0-9]{4})/
+	/([0-9]{1,2}\.\s*(?:Januar|Februar|März|April|Mai|Juni|Juli|August|September|Oktober|November|Dezember|[0-9]{1,2})\.\s*[0-9]{4})/,
 ]);
 
 function* check(data, tm) {
@@ -19,6 +19,11 @@ function* check(data, tm) {
 	}
 
 	const newest_comment = verlegt_comments[verlegt_comments.length - 1];
+	const DISABLED = true;
+	if (DISABLED) {
+		return;
+	}
+	yield newest_comment;
 	// TODO check for comments
 	//console.error(newest_comment);
 }

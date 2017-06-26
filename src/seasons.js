9efe50c90219e8607c2f3230d860c07c4be41c1f
Philipp Hagemeister <phihag@phihag.de>
2017-06-26 14:33:43 +0200

    season creation: allow inputting the ID

--- src/seasons.js
+++ src/seasons.js
@@ -9,7 +9,10 @@ const kc_email = require('./kc_email');
 
 
 function add_handler(req, res, next) {
-	const m = /.*\?id=([A-Za-z0-9-]{36})$/.exec(req.body.url);
+	let m = /.*\?id=([A-Za-z0-9-]{36})$/.exec(req.body.url);
+	if (!m) {
+		m = /^([A-Za-z0-9-]{36})$/.exec(req.body.url);
+	}
 	if (!m) {
 		return next(new Error('cannot find tournament ID'));
 	}

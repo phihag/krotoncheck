23a0e6e39cf6753f365751c12c9637a3791a08a1
Philipp Hagemeister <phihag@phihag.de>
2017-10-17 13:43:10 +0200

    nichtantritt: require any of a number of OGs

--- src/checks/nichtantritt.js
+++ src/checks/nichtantritt.js
@@ -8,18 +8,16 @@ module.exports = function*(season) {
 			continue;
 		}
 
-		if (! data.get_stb_note(tm.matchid, note_text => /F(?:01|37)/.test(note_text))) {
+		if (! data.get_stb_note(tm.matchid, note_text => /F(?:01|13|14|15|37|40)/.test(note_text))) {
 			const contains_o = !!data.get_stb_note(tm.matchid, note_text => /FO1/.test(note_text));
-			const contains_f13 = !!data.get_stb_note(tm.matchid, note_text => /F13/.test(note_text));
 			const contains_og = !!data.get_stb_note(tm.matchid, note_text => /OG|Ordnungsgebühr/.test(note_text));
 
 			const message = (
-				'Mannschaftsspiel ohne Kampf, aber Ordnungsgebühr F01 oder F37 gegen ' +
+				'Mannschaftsspiel ohne Kampf, aber dementsprechende Ordnungsgebühr (z.B. F01 oder F37) gegen ' +
 				(tm.flag_ok_gegen_team1 ? 'Heimmannschaft (' + tm.team1name + ')' : 'Gastmannschaft (' + tm.team2name + ')')
 				+ ' fehlt.' +
 				(contains_o ? ' (F01 mit o statt Null geschrieben?)' : '') +
-				((contains_og && !contains_f13) ? ' (Ordnungsgebühr-Kennung F01/F37 vergessen?)' : '') +
-				(contains_f13 ? ' (Ordnungsgebühr F13 verhängt)' : '')
+				(contains_og ? ' (Ordnungsgebühr-Kennung F01/F37 vergessen?)' : '')
 			);
 			yield {
 				teammatch_id: tm.matchid,
